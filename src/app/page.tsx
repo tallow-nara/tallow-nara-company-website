71a091bf5d299040a28745b9fb09457731ebda83
faizalfakhri0001 <faizalfakhri0001@gmail.com>
2025-11-09 16:07:21 +0700

    next: phase 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { CTASection } from "@/sections/CTASection";
 import { CommitmentSection } from "@/sections/CommitmentSection";
 import { FooterSection } from "@/sections/FooterSection";
 import { HeroSection } from "@/sections/HeroSection";
-import { IngredientsImpactSection } from "@/sections/IngredientsImpactSection";
 import { ProductHighlightSection } from "@/sections/ProductHighlightSection";
-import { StorySection } from "@/sections/StorySection";
 import { TestimonialsSection } from "@/sections/TestimonialsSection";
 import { WhyTallownaraSection } from "@/sections/WhyTallownaraSection";
 
@@ -14,9 +12,7 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
-      <StorySection />
       <WhyTallownaraSection />
-      <IngredientsImpactSection />
       <ProductHighlightSection />
       <CommitmentSection />
       <TestimonialsSection />
